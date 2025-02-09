991cd8e35ed656fdbc25ce445e4690f8912e3b6e
Luminaw <146209657+Luminaw@users.noreply.github.com>
2025-02-09 11:42:23 -0500

    partial theming

--- assets/js/app.js
+++ assets/js/app.js
@@ -21,6 +21,7 @@ import "phoenix_html"
 import {Socket} from "phoenix"
 import {LiveSocket} from "phoenix_live_view"
 import topbar from "../vendor/topbar"
+import '@unocss/runtime'
 
 let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
 let liveSocket = new LiveSocket("/live", Socket, {
@@ -41,4 +42,14 @@ liveSocket.connect()
 // >> liveSocket.enableLatencySim(1000)  // enabled for duration of browser session
 // >> liveSocket.disableLatencySim()
 window.liveSocket = liveSocket
+
+// load unocss
+fetch('https://cdn.jsdelivr.net/npm/@unocss/runtime')
+  .then(response => response.text())
+  .then(code => {
+    const script = document.createElement('script');
+    script.textContent = code;
+    document.head.appendChild(script);
+  });
+
 
