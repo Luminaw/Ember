c3652ad26571767c12c137dacb65d3e10c48105f
Luminaw <146209657+Luminaw@users.noreply.github.com>
2025-02-11 22:32:35 -0500

    fix esbuild

--- assets/build.js
+++ assets/build.js
@@ -1,28 +1,12 @@
 import * as esbuild from 'esbuild'
-import { createGenerator } from 'unocss'
-import unoconfig from './uno.config.js'
-
-const uno = createGenerator(unoconfig)
-
-const unocssPlugin = {
-  name: 'unocss',
-  setup(build) {
-    build.onLoad({ filter: /\.js$/ }, async (args) => {
-      const { css } = await uno.generate(args.path)
-      return {
-        contents: css,
-        loader: 'css'
-      }
-    })
-  }
-}
 
 await esbuild.build({
   entryPoints: ['js/app.js'],
   bundle: true,
   outdir: '../priv/static/assets',
-  plugins: [unocssPlugin],
+  plugins: [cssPlugin],
   loader: {
-    '.js': 'jsx'
-  }
+    '.js': 'jsx',
+    '.css': 'css',
+  },
 })
